67fcd71e62ea4f1e9e566e11c5acdca9c4bc3475
BLOCXTECH <134414477+BLOCXTECH@users.noreply.github.com>
2023-07-16 13:24:27 +0300

    Add files via upload

---
+++ Mining Softwares/cpuminer-multi-rel1.3.1-x64/RUN-BLOCX X11.bat
@@ -0,0 +1,3 @@
+cpuminer-gw64-corei7 -a x11 -o stratum+tcp://157.230.28.149:3008 -u yourwalletaddress -p yourpassword
+
+pause
---
+++ Mining Softwares/cpuminer-multi-rel1.3.1-x64/mine.bat
@@ -0,0 +1,1 @@
+cpuminer-gw64-corei7.exe -a x11 -o stratum+tcp://157.230.28.149:3008 -u B7CjqANX7heHnZoyhr2DLiDycZMPLqxWyq -p anything
