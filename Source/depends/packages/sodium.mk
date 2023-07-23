d9459cbb2d042abd91baba103f451c2eb17af23d
BLOCXTECH <contact@bloxc.tech>
2023-07-23 08:39:19 +0300

    depends packages

--- Source/depends/packages/sodium.mk
+++ Source/depends/packages/sodium.mk
@@ -2,7 +2,7 @@ package=sodium
 $(package)_version=1.0.18
 $(package)_download_path=https://download.libsodium.org/libsodium/releases
 $(package)_file_name=libsodium-$($(package)_version)-stable.tar.gz
-$(package)_sha256_hash=4080b5bf724c622febdfda0e82ded8bd5b1b889afe4a39e49d13bc974d325a6d
+$(package)_sha256_hash=E78F7FEDEC1D803AE832D4DFF9A6054ACA3040348AF4938031E7871C5A35449C
 
 define $(package)_set_vars
 $(package)_config_opts+=--with-pic --disable-shared
